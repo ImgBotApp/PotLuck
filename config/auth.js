7257939240e6e0e527453dcbcd38b2593aecd9e8
Omar Taylor <omtaylor@iastate.edu>
2016-11-18 01:48:22 -0600

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '1166847693395202',
-        'clientSecret'  : 'e2ccb2501a4948903140796cd8940e5d',
+        'clientID'      : 'ID',
+        'clientSecret'  : 'Secret',
         'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
     },
 
