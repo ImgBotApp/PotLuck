62574450778e850d0fe915fe73619f9311d76fe3
Omar Taylor <omtaylor@iastate.edu>
2017-01-24 14:30:35 -0600

    put back auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,29 +1,29 @@
 /**
- * Created by O on 11/11/2016.
+ * Created by omarc on 1/24/2017.
  */
 module.exports = {
 
     'facebookAuth' : {
         'clientID': '1917972928433923',
-        'clientSecret': '347d795c40db700207577020b407755b',
+        'clientSecret': 'ff489bc0679fa658fe828cea823658b3',
         'callbackURL': 'http://162.243.2.42:8080/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey': '4abEL9bwrkp0VZFSiXrF4OaKM',
-        'consumerSecret': 'DhuhSf9RfVq12fXkEYGTQzQrVJhkbcM3TEuXA3j7P7LRbR9AWq',
+        'consumerKey': 't4mfW35fR3BXYPG1NvLA4CI4d',
+        'consumerSecret': 'VzynJcRFhNhxXtaVdLw0DHejszPG87Bo2LTbToJxE1O9PuwZhs',
         'callbackURL': 'http://162.243.2.42:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID': '247802489669-pfma4ktit194a544er8jmf7fvu8q2k6u.apps.googleusercontent.com',
-        'clientSecret': '91ferE0ShGcZfhbgrXjEw9mi',
+        'clientSecret': '5p6CI4b-uInsmznoQIoeGu9_',
         'callbackURL': 'http://162.243.2.42:8080/auth/google/callback'
     },
 
     'githubAuth' : {
         'clientID': '3dd1907c1afb78999fdf',
-        'clientSecret': '2d9cda0608e2e42baec588ad6830b563c2b8405a',
+        'clientSecret': 'fb3ac8c65c63396f5fcc946caf10b3e9ca28a161',
         'callbackURL': 'http://162.243.2.42:8080/auth/github/callback'
     }
 
