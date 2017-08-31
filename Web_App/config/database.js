630b16fb195b906d816cb9de881f808afb5f32bb
Omar Taylor <omtaylor@iastate.edu>
2017-08-31 17:57:53 -0500

    commited gibrish on accident. fixed it

--- Web_App/config/database.js
+++ Web_App/config/database.js
@@ -1,7 +1,7 @@
 /**
  * Created by O on 10/13/2016.
  */
-const dbURL = 'mongodb://isu-dsl@fhsjafhd162.243.2.42:27017/PotLuck';
+const dbURL = 'mongodb://162.243.2.42:27017/PotLuck';
 const dbName = dbURL.split('/');
 
 module.exports = {
